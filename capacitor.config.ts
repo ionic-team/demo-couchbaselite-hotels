3acdc89f2aebb9176d8fd88888ae8b1742934018
Matt Netkow <matt.netkow@gmail.com>
2021-10-06 14:59:41 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demo-couchbaselite-hotels',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
