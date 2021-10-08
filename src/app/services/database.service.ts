bc8013303702b835a4da5115f457d1b8d4f21099
Matt Netkow <matt.netkow@gmail.com>
2021-10-07 21:35:25 -0500

    updates

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import {
-  CordovaEngine,
   Database,
   DatabaseConfiguration,
   DataSource,
-  IonicCBL,
   Meta,
   MutableDocument,
   Ordering,
@@ -14,46 +12,42 @@ import {
   Expression
 } from '@ionic-enterprise/couchbase-lite';
 import { Hotel } from '../models/hotel';
-import { Directory, Filesystem } from '@capacitor/filesystem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
   private database: Database;
-  private readyPromise: Promise<void>;
   public hotels: Hotel[] = [];
 
   constructor() {
-    this.readyPromise = this.initializeDatabase();
+    this.initializeDatabase();
   }
 
   private async initializeDatabase(): Promise<void> {
-    return new Promise(resolve => {
-      IonicCBL.onReady(async () => {
-        const config = new DatabaseConfiguration();
-        //config.setEncryptionKey('8e31f8f6-60bd-482a-9c70-69855dd02c38');
-        //this.database = new Database('travel-sample', config);
-        // this.database.setEngine(
-        //   new CordovaEngine({
-        //     allResultsChunkSize: 9999
-        //   })
-        // );
+    const config = new DatabaseConfiguration();
+    //config.setEncryptionKey('8e31f8f6-60bd-482a-9c70-69855dd02c38');
+    //this.database = new Database('travel-sample', config);
+    // this.database.setEngine(
+    //   new CordovaEngine({
+    //     allResultsChunkSize: 9999
+    //   })
+    // );
 
-        try {
-        this.database.copy("/assets/db.sqlite3", 'travel-sample', config);
-        console.log("opened DB!");
-        } catch (e) {
-        console.log('Could not load pre-built database');
-      }
-              
-        resolve();
-      });
-    });
+    // try {
+    //   const file = await Filesystem.getUri({
+    //     directory: Directory.Data,
+    //     path: "db.sqlite3"
+    //   });
+
+    //   this.database.copy(file.uri, 'travel-sample', config);
+    //   console.log("opened DB!");
+    // } catch (e) {
+    //   console.log('Could not load pre-built database');
+    // }
   }
 
   public async getHotels() {
-    await this.readyPromise;
     return this.hotels;
   }
 
