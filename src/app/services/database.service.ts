c4b89f7201bb31a2f826f8bd3ff961015d7522bf
Matt Netkow <matt.netkow@gmail.com>
2021-10-10 13:57:06 -0500

    cbl refinements

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,91 +1,79 @@
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
-// import {
-//   Database,
-//   DatabaseConfiguration,
-//   DataSource,
-//   Meta,
-//   MutableDocument,
-//   Ordering,
-//   QueryBuilder,
-//   SelectResult,
-//   Expression
-// } from '@ionic-enterprise/couchbase-lite';
-import { hotelData } from '../data/hotels';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import {
+  Database,
+  DatabaseConfiguration,
+  DataSource,
+  Meta,
+  MutableDocument,
+  Ordering,
+  QueryBuilder,
+  SelectResult,
+  Expression
+} from '@ionic-enterprise/couchbase-lite';
 import { Hotel } from '../models/hotel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
-  //private database: Database;
-  public hotels: Hotel[] = [];
+  private database: Database;
+  private hotels: Hotel[] = [];
 
-  constructor() {
-    this.hotels = hotelData;
-    this.initializeDatabase();
-  }
+  constructor() { }
 
-  private async initializeDatabase(): Promise<void> {
-    //const config = new DatabaseConfiguration();
-    //config.setEncryptionKey('8e31f8f6-60bd-482a-9c70-69855dd02c38');
-    //this.database = new Database('travel-sample', config);
-    // this.database.setEngine(
-    //   new CordovaEngine({
-    //     allResultsChunkSize: 9999
-    //   })
-    // );
+  private async initializeDatabase(): Promise<Hotel[]> {
+    // When on iOS/Android, load the Couchbase Lite travel database used in many of their tutorials.
+    if (Capacitor.isNativePlatform()) {
+      const config = new DatabaseConfiguration();
+      
+      /* Note about encryption: In a real-world app, the encryption key should not be hardcoded like it is here. 
+         One strategy is to auto generate a unique encryption key per user on initial app load, then store it securely in the device's keychain for later retrieval.
+         Ionic's Identity Vault (https://ionicframework.com/docs/enterprise/identity-vault) plugin is an option. Using IV’s storage API, you can ensure that the 
+         key cannot be read or accessed without the user being authenticated first. */
+      config.setEncryptionKey('8e31f8f6-60bd-482a-9c70-69855dd02c38');
 
-    // try {
-    //   const file = await Filesystem.getUri({
-    //     directory: Directory.Data,
-    //     path: "db.sqlite3"
-    //   });
-
-    //   this.database.copy(file.uri, 'travel-sample', config);
-    //   console.log("opened DB!");
-    // } catch (e) {
-    //   console.log('Could not load pre-built database');
-    // }
-  }
-
-  /*
-    Import static hotel data into a large JSON object (data/hotels.ts), then load into Couchbase.
-    In future, a Couchbase Lite database zip file will be shipped with the app binary,
-    then extracted and loaded upon first time app initialization.
-  */
-    private async seedInitialData() {
-      // let count = await this.getDatabaseCount();
-      // if (count === 0) {
-      //   for (let hotel of hotelData) {
-      //     let doc = new MutableDocument()
-      //       .setString('name', hotel.name)
-      //       .setString('address', hotel.address)
-      //       .setString('phone', hotel.phone);
-          
-      //     this.database.save(doc);
-      //   }
-      // }
+      try {
+        if (!this.database.exists("travel-sample", Directory.Data)) {
+          const travelDbFile = await Filesystem.getUri({
+            directory: Directory.Data,
+            path: "db.sqlite3"
+          });
+  
+          this.database.copy(travelDbFile.uri, 'travel-sample', config);
+        } else {
+          this.database = new Database("travel-sample", config);
+        }
+      } catch (e) {
+        console.log('Could not load pre-built database.');
+      }
+    }
+    else {
+      // When running on the web, use hotel data from a file
+      const hotelFile = await import("../data/hotels");
+      this.hotels = hotelFile.hotelData;
     }
 
-  public async getHotels() {
     return this.hotels;
   }
 
+  public async getHotels() {
+    return await this.initializeDatabase();
+  }
+
+  
+
+  
+  
+  
+  
+  
+  
   public async filterData(hotelName: string) {
     const filtered = this.hotels.filter(
       h => h.name.toLowerCase().includes(hotelName.toLowerCase()));
 
     return filtered;
   }
-
-  // private async getDatabaseCount() {
-  //   const query = QueryBuilder.select(SelectResult.all())
-  //     .from(DataSource.database(this.database));
-    
-  //   const result = await query.execute();
-  //   const count = (await result.allResults()).length;
-  //   return count;
-  // }
-
 }
